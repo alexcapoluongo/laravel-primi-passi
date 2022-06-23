56513b053a537f00269f4df3c506fc63cd34d2ac
alexcapoluongo <alexcapoluongo@icloud.com>
2022-06-23 15:23:05 +0200

    added style.css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,20 @@
-<ul>
-@foreach ($menu as $item)
-<li><a href="{{ $item['viewLink'] }}"> {{ $item['name'] }} </li>
-@endforeach
-</ul>
 
 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+    <ul>
+        @foreach ($menu as $item)
+        <li><a href="{{ $item['viewLink'] }}"> {{ $item['name'] }} </li>
+        @endforeach
+    </ul>
+</body>
+</html>
